d7950887d74d09eea35c122d165e5b60ff8701c7
JOAO CRUZ <jsabino765@gmail.com>
2023-03-14 17:18:28 -0300

    First Commit

---
+++ src/br/com/dio/teste.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class teste {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+	System.out.println(gato);
+    System.out.println(livro);
+    
+		/*int a = 10;
+		int b = 10;
+		
+	System.out.println("O Resultado é:  " + (a + b + 2));*/
+
+	}
+
+}
+class Livros{
+	private String nome;
+	private String npag;
+	
+}
